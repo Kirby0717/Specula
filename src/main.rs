289694da7fff241c8c0cd12d00ed329f01893438
Kirby0717 <78634415+Kirby0717@users.noreply.github.com>
2026-03-21 21:58:44 +0900

    シェル終了でターミナルも終了 (#13)

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,7 @@ fn main() -> anyhow::Result<()> {
 
     gui::run_app()?;
 
-    return Ok(());
-
-    /*let (mut terminal, handle) =
-        terminal::Terminal::new(10, 30, 1_000_000, "bash")?;
-
-    for _ in 0..15 {
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        terminal.process_pty_output();
-    }
-
-    println!("send echo hello");
-    terminal.write(b"echo hello\r");
-
-    for _ in 0..15 {
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        terminal.process_pty_output();
-    }
-
-    println!("~~~~~~~~~~");
-    println!("{}", terminal.core.dump_visible());
-
-    terminal.write(b"exit\r");
-    std::thread::sleep(std::time::Duration::from_millis(100));
-    terminal.process_pty_output();
-
-    terminal.pty.wait()?;
-    handle.join().ok();
-    Ok(())*/
+    Ok(())
 }
 
 fn log_init() {
