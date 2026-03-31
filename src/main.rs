6c12895d512240bb89f5da10d2ad028c7c7e59d3
Kirby0717 <78634415+Kirby0717@users.noreply.github.com>
2026-04-01 02:51:56 +0900

    ログレベルの設定を可能に (#55)

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,29 @@ mod core;
 mod gui;
 
 fn main() -> anyhow::Result<()> {
-    log_init();
-    setup_panic_hook();
-
     let config = config::Config::load().unwrap_or_default();
+
+    log_init(config.log_level);
+    setup_panic_hook();
 
     gui::run_app(config)?;
 
     Ok(())
 }
 
-fn log_init() {
+fn log_init(log_level: log::LevelFilter) {
     use simplelog::{
-        CombinedLogger, Config, LevelFilter, SharedLogger, SimpleLogger,
-        WriteLogger,
+        CombinedLogger, Config, SharedLogger, SimpleLogger, WriteLogger,
     };
 
     let mut loggers: Vec<Box<dyn SharedLogger>> = vec![];
     loggers.push(WriteLogger::new(
-        LevelFilter::Info,
+        log_level,
         Config::default(),
         std::fs::File::create("specula.log").unwrap(),
     ));
     if cfg!(debug_assertions) {
-        loggers.push(SimpleLogger::new(LevelFilter::Info, Config::default()));
+        loggers.push(SimpleLogger::new(log_level, Config::default()));
     }
     CombinedLogger::init(loggers).unwrap();
 }
